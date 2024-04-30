4465b89ff0802a93de22aa00d637570dce8ca991
Radisson Kouhi <radissonkouhi@pc41.wireless12.tku.edu.tw>
2024-04-30 14:27:54 +0800

    change the file

--- hello.py
+++ hello.py
@@ -1,1 +1,4 @@
-print("hello world.")
+from name import square
+
+for i in range(10):
+    print(f"Hello world.{square(i)}")
--- name.py
+++ name.py
@@ -1,12 +1,27 @@
-def announce(f):
-    def wrapper():
-        print("before ... ")
-        f()
-        print("after.")
-    return wrapper
+def copy_times(name, number):
+    if number <= 0:
+        print("You entered a non-positive number.")
+        answer = input("Do you want to try again? [Y/n]")
+        if answer.lower() == "y":
+            return True
+        else:
+            return False
+    else:
+        for i in range(number):
+            print(f"{i}:Hello dear {name}. the square of {i} is {square(i)}")
 
-@announce
-def hello():
-    print("hello world.")
+def square(x):
+    return x * x
 
-hello()
+def main():
+    name = input("Enter your name: ")
+    while True:
+        try:
+            number = int(input("How many times do you want to print? "))
+            if not copy_times(name, number):
+                break
+        except ValueError:
+            print("Please enter a valid integer.")
+
+if __name__ == "__main__":
+    main()
